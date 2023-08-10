31013def78007083b36acb38f3a5703d6d7eaf79
tksassano <113140378+tksassano@users.noreply.github.com>
2023-08-09 22:41:09 -0400

    Added quantification

--- ngs_rna.py
+++ ngs_rna.py
@@ -6,25 +6,18 @@ import ngs_classes
 from ngs_functions import print_log, run_command
 ######################################################
 
-def ngs_rna_index(inputs):
-    print_log(inputs, "\n<---> RNA Indexing <--->\n")
-    runDir = os.path.abspath(inputs.dir + '/RNA_SEQ/')
-    error_msg = "[ERROR] Failed to perform RNA indexing."
-    if(os.path.exists(runDir + '/Genome')):
-        print_log(inputs, "[INFO] Indices found, skipping.")
-        return 0
-    else:
-        run_command(inputs, "%s --runThreadN %d --runMode genomeGenerate --genomeDir %s --genomeFastaFiles %s --sjdbGTFfile %s" % (inputs.star, inputs.threads, runDir, inputs.reference, inputs.annotation), error_msg)
-    return 0
+def ngs_quantification(inputs):
 
-def ngs_rna_align(inputs, targets_data):
-    print_log(inputs, "\n<---> RNA Alignment <--->\n") 
+    print_log(inputs, "\n<---> RNA Quantification <--->\n")
     runDir = os.path.abspath(inputs.dir + '/RNA_SEQ/')
-    tempDir = "~/star_temp/"
-    r1 = ",".join([s._trimmedR1 for s in targets_data])
-    r2 = ",".join([s._trimmedR2 for s in targets_data])
-    readFilesIn = " ".join([r1, r2])
-    error_msg = "[ERROR] Failed to perform RNA alignment."
-    run_command(inputs, "%s --runThreadN %d --genomeDir %s --readFilesIn %s --outTmpDir %s --readFilesCommand zcat" % (inputs.star, inputs.threads, runDir, readFilesIn, tempDir), error_msg)
 
+    aligned = [f for f in os.listdir(runDir) if f.endswith('Aligned.toTranscriptome.out.bam')]
+    
+    for sample in aligned:
+        sample_id = sample.split('Aligned.toTranscriptome.out.bam')[0] 
+        outFile = os.path.join(runDir, f"Quant_{sample_id}")
+        
+        error_msg = f"[ERROR] Failed to perform RNA quantification for sample {sample_id}."
+        run_command(inputs, "salmon quant --posBias --gcBias --seqBias --threads %d --libType ISR -t %s -a %s -o %s --numBootstraps 100" % (inputs.threads, inputs.reference, sample, outFile), error_msg)
+    
     return 0
