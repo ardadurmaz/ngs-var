7053cb3cb7116752e9f3c62ea89a3bfd797bf0e2
tksassano <113140378+tksassano@users.noreply.github.com>
2023-08-19 14:31:05 -0400

    Converted to f string format

--- ngs_preprocess.py
+++ ngs_preprocess.py
@@ -12,7 +12,7 @@ def ngs_index(inputs):
 		if(len(glob.glob(os.path.abspath(inputs.reference + '.bwt'))) > 0):
 			print_log(inputs, "[INFO] Indices found, skipping.")
 		else:
-			run_command(inputs, "%s index %s" % (inputs.bwa,inputs.reference), "[ERROR:BWA] Failed to create fasta index")
+			run_command(inputs, f"{inputs.bwa} index {inputs.reference}", "[ERROR:BWA] Failed to create fasta index")
 	elif re.search("star", str.lower(inputs.aligner)):
 		print_log(inputs, "\n<---> RNA Indexing <--->\n")
 		runDir = os.path.abspath(inputs.dir + '/RNA_SEQ/')
@@ -21,7 +21,7 @@ def ngs_index(inputs):
 			print_log(inputs, "[INFO] Indices found, skipping.")
 			return 0
 		else:
-			run_command(inputs, "%s --runThreadN %d --runMode genomeGenerate --genomeDir %s --genomeFastaFiles %s --sjdbGTFfile %s" % (inputs.star, inputs.threads, runDir, inputs.reference, inputs.annotation), error_msg)
+			run_command(inputs, f"{inputs.star} --runThreadN {inputs.threads} --runMode genomeGenerate --genomeDir {runDir} --genomeFastaFiles {inputs.reference} --sjdbGTFfile {inputs.annotation}", error_msg)
 	return True
 
 def ngs_trim(inputs, targets_data):
@@ -33,7 +33,7 @@ def ngs_trim(inputs, targets_data):
 			if(os.path.exists(trimmedR1_Name) and os.path.exists(trimmedR2_Name)):
 				print_log(inputs, "[INFO] Trimmed reads found, skipping")	
 			else:
-				run_command(inputs, "%s --thread 12 --compression 9 --trim_front1 10 --trim_front2 10 --cut_tail --detect_adapter_for_pe --in1 %s --in2 %s --out1 %s --out2 %s" % (inputs.fastp,r._r1,r._r2, trimmedR1_Name,trimmedR2_Name), "[ERROR:CUTADAPT] Failed to trim reads")
+				run_command(inputs, f"{inputs.fastp} --thread 12 --compression 9 --trim_front1 10 --trim_front2 10 --cut_tail --detect_adapter_for_pe --in1 {r._r1} --in2 {r._r2} --out1 {trimmedR1_Name} --out2 {trimmedR2_Name}", "[ERROR:CUTADAPT] Failed to trim reads")
 			r._trimmedR1 = trimmedR1_Name
 			r._trimmedR2 = trimmedR2_Name
 	elif re.search('cutadapt', str.lower(inputs.trimmer)):
@@ -44,7 +44,7 @@ def ngs_trim(inputs, targets_data):
 			if(os.path.exists(trimmedR1_Name) and os.path.exists(trimmedR2_Name)):
 				print_log(inputs, "[INFO] Trimmed reads found, skipping")	
 			else:
-				run_command(inputs, "%s -q 15,10 -A XXX -m 30 -o %s -p %s %s %s" % (inputs.cutadapt,trimmedR1_Name,trimmedR2_Name,r._r1,r._r2), "[ERROR:CUTADAPT] Failed to trim reads")
+				run_command(inputs, f"{inputs.cutadapt} -q 15,10 -A XXX -m 30 -o {trimmedR1_Name} -p {trimmedR2_Name} {r._r1} {r._r2}", "[ERROR:CUTADAPT] Failed to trim reads")
 			r._trimmedR1 = trimmedR1_Name
 			r._trimmedR2 = trimmedR2_Name
 			
@@ -56,7 +56,7 @@ def ngs_trim(inputs, targets_data):
 			os.path.exists(os.path.abspath(inputs.dir + '/readsTrimmed/' + stagr2 + '.good.fq'))):
 				print_log(inputs, "    *** trimmed reads found, skipping...")
 			else:
-				run_command(inputs, "python %s -1 %s -2 %s --debubble False -g %s --no_correction" % (inputs.afterqc,r._r1, r._r2,os.path.abspath(inputs.dir + '/readsTrimmed/')), "[ERROR:AFTERQC] Failed to trim reads")
+				run_command(inputs, f"python {inputs.afterqc} -1 {r._r1} -2 {r._r2} --debubble False -g {os.path.abspath(inputs.dir + '/readsTrimmed/')} --no_correction", "[ERROR:AFTERQC] Failed to trim reads")
 			r._trimmedR1 = os.path.abspath(inputs.dir + '/readsTrimmed/' + stagr1 + '.good.fq')
 			r._trimmedR2 = os.path.abspath(inputs.dir + '/readsTrimmed/' + stagr2 + '.good.fq')
 	
@@ -69,7 +69,7 @@ def ngs_trim(inputs, targets_data):
 			if(os.path.exists(trimmedR1_Name) and os.path.exists(trimmedR2_Name)):
 				print_log(inputs, "[INFO] Trimmed reads found, skipping.")
 			else:
-				run_command(inputs, "%s -m pe -q 10 -Q 20 -l 35 -n -t %d -o %s %s %s" % (inputs.skewer,inputs.threads,trimmed_Name,r._r1,r._r2), "[ERROR:SKEWER] Failed to trim reads %s %s" % (r._r1,r._r2))
+				run_command(inputs, f"{inputs.skewer} -m pe -q 10 -Q 20 -l 35 -n -t {inputs.threads} -o {trimmed_Name} {r._r1} {r._r2}", f"[ERROR:SKEWER] Failed to trim reads {r._r1} {r._r2}")
 			r._trimmedR1 = trimmedR1_Name
 			r._trimmedR2 = trimmedR2_Name
 			
@@ -82,9 +82,7 @@ def ngs_align(inputs, targets_data):
 			if(os.path.exists(aligned_Name)):
 				print_log(inputs, "[INFO] Aligned reads found, skipping")
 			else:
-				run_command(inputs, "/bin/bash -c '%s mem -t %d -M -R \"@RG\\tID:%s\\tSM:%s\\tPL:%s\\tLB:%s\" %s <(gunzip -c %s) <(gunzip -c %s) | samtools sort -@%d -l9 -o %s'" % (inputs.bwa,inputs.threads,
-						r._id + '_' + r._lib + '_' + r._lane,r._id,str.upper(r._plat),r._lib,
-						inputs.reference,r._trimmedR1,r._trimmedR2, inputs.threads, aligned_Name), "[ERROR:BWA] Failed to align reads %s %s" % (r._trimmedR1, r._trimmedR2))
+				run_command(inputs, f"/bin/bash -c '{inputs.bwa} mem -t {inputs.threads} -M -R \"@RG\\tID:{r._id}_{r._lib}_{r._lane}\\tSM:{r._id}\\tPL:{str.upper(r._plat)}\\tLB:{r._lib}\" {inputs.reference} <(gunzip -c {r._trimmedR1}) <(gunzip -c {r._trimmedR2}) | samtools sort -@{inputs.threads} -l9 -o {aligned_Name}'", f"[ERROR:BWA] Failed to align reads {r._trimmedR1} {r._trimmedR2}")
 			r._aligned = aligned_Name
 	elif re.search("star", str.lower(inputs.aligner)):
 		print_log(inputs, "\n<---> RNA Alignment <--->\n") 
@@ -105,7 +103,7 @@ def ngs_align(inputs, targets_data):
 			readFilesIn = " ".join([r1, r2])
 
 			error_msg = f"[ERROR] Failed to perform RNA alignment for sample {sample_id}."
-			run_command(inputs, "%s --runThreadN %d --genomeDir %s --readFilesIn %s --outTmpDir %s --readFilesCommand zcat --twopassMode Basic --quantMode TranscriptomeSAM --outSAMtype BAM Unsorted --outBAMcompression 10 --outFileNamePrefix %s" % (inputs.star, inputs.threads, runDir, readFilesIn, tempDir, sampleName), error_msg)
+			run_command(inputs, f"{inputs.star} --runThreadN {inputs.threads} --genomeDir {runDir} --readFilesIn {readFilesIn} --outTmpDir {tempDir} --readFilesCommand zcat --twopassMode Basic --quantMode TranscriptomeSAM --outSAMtype BAM Unsorted --outBAMcompression 10 --outFileNamePrefix {sampleName}", error_msg)
 	return True
 
 def ngs_mark_sort(inputs, targets_data):
@@ -117,7 +115,7 @@ def ngs_mark_sort(inputs, targets_data):
 				print_log(inputs, "[INFO] Processed bam files found, skipping.")
 			else:
 				## Remove Duplicates ##
-				run_command(inputs, "%s markdup --overflow-list-size=800000 --io-buffer-size=512 -l9 -p -t %d %s %s" % (inputs.sambamba,inputs.threads,r._aligned,rmdup_Name), "[ERROR:SAMBAMBA] Failed to mark&remove duplicates in .bam file")
+				run_command(inputs, f"{inputs.sambamba} markdup --overflow-list-size=800000 --io-buffer-size=512 -l9 -p -t {inputs.threads} {r._aligned} {rmdup_Name}", "[ERROR:SAMBAMBA] Failed to mark&remove duplicates in .bam file")
 			r._processed = rmdup_Name
 	
 	return True
@@ -141,25 +139,25 @@ def ngs_merge(inputs,targets_data,targets_data_processed):
 		for s in sample_ids:
 			## TUMOR ##
 			if(len(sample_ids[s]["TUMOR"]) == 1):
-				print_log(inputs, "[INFO] Single bam file for sample %s skipping." % (s))
+				print_log(inputs, f"[INFO] Single bam file for sample {s} skipping.")
 				merged_tumor_bam_name = sample_ids[s]["TUMOR"][0]
 			else:
 				merged_tumor_bam_name = os.path.abspath(inputs.dir + '/bamMerged/Merged_Tumor_' + s + '.bam')
 				if(os.path.exists(merged_tumor_bam_name)):
 					print_log(inputs, "[INFO] Merged .bam file found for tumor sample, skipping.")
 				else:
-					run_command(inputs, "%s merge -l9 -t %d %s %s" % (inputs.sambamba,inputs.threads,merged_tumor_bam_name,' '.join(sample_ids[s]["TUMOR"])), "[ERROR:SAMBAMBA] Failed to merge files %s" % (' '.join(sample_ids[s]["TUMOR"])))
+					run_command(inputs, f"{inputs.sambamba} merge -l9 -t {inputs.threads} {merged_tumor_bam_name} {' '.join(sample_ids[s]['TUMOR'])}", f"[ERROR:SAMBAMBA] Failed to merge files {' '.join(sample_ids[s]['TUMOR'])}")
 
 			## NORMAL ##
 			if(len(sample_ids[s]["NORMAL"]) == 1):
-				print_log(inputs, "[INFO] Single bam file for sample %s skipping." % (s))
+				print_log(inputs, f"[INFO] Single bam file for sample {s} skipping.")
 				merged_normal_bam_name = sample_ids[s]["NORMAL"][0]
 			else:
 				merged_normal_bam_name = os.path.abspath(inputs.dir + '/bamMerged/Merged_Normal_' + s + '.bam')
 				if(os.path.exists(merged_normal_bam_name)):
 					print_log(inputs, "[INFO] Merged .bam file found for normal sample, skipping")
 				else:
-					run_command(inputs, "%s merge -l9 -t %d %s %s" % (inputs.sambamba,inputs.threads,merged_normal_bam_name,' '.join(sample_ids[s]["NORMAL"])), "[ERROR:SAMBAMA] Failed to merge files %s" % (' '.join(sample_ids[s]["TUMOR"])))
+					run_command(inputs, f"{inputs.sambamba} merge -l9 -t {inputs.threads} {merged_normal_bam_name} {' '.join(sample_ids[s]['NORMAL'])}", f"[ERROR:SAMBAMA] Failed to merge files {' '.join(sample_ids[s]['TUMOR'])}")
 			targets_data_processed.append(ngs_classes.WxsSample(_id=s,_bamTumor = merged_tumor_bam_name,_bamNormal=merged_normal_bam_name))            
 	elif str.upper(inputs.workflow) == "GERMLINESNVINDEL":
 		sample_ids = {}
@@ -171,14 +169,14 @@ def ngs_merge(inputs,targets_data,targets_data_processed):
 				sample_ids[r._id].append(r._processed)
 		for s in sample_ids:
 			if(len(sample_ids[s]) == 1):
-				print_log(inputs, "[INFO] Single bam file for sample %s skipping." % (s))
+				print_log(inputs, f"[INFO] Single bam file for sample {s} skipping.")
 				merged_bam_name = sample_ids[s][0]
 			else:
 				merged_bam_name = os.path.abspath(inputs.dir + '/bamMerged/Merged_' + s + '.bam')
 				if(os.path.exists(merged_bam_name)):
 					print_log(inputs, "[INFO] Merged bam files found, skipping.")
 				else:
-					run_command(inputs, "%s merge -l9 -t %d %s %s" % (inputs.sambamba,inputs.threads,merged_bam_name,' '.join(sample_ids[s])), "[ERROR:SAMBAMBA] Failed to merge files %s" % (' '.join(sample_ids[s])))
+					run_command(inputs, f"{inputs.sambamba} merge -l9 -t {inputs.threads} {merged_bam_name} {' '.join(sample_ids[s])}", f"[ERROR:SAMBAMBA] Failed to merge files {' '.join(sample_ids[s])}")
 			targets_data_processed.append(ngs_classes.WxsSample(_id = s,_bam = merged_bam_name))
 	
 	return 0
@@ -187,79 +185,87 @@ def bqsr(inputs, targets_data_processed):
 	
 	for s in targets_data_processed:
 		if str.upper(inputs.workflow) == "SOMATICSNVINDEL":
-			if(len(glob.glob(os.path.abspath(s._bamTumor + '.bai'))) > 0):
+			if len(glob.glob(os.path.abspath(f"{s._bamTumor}.bai"))) > 0:
 				print_log(inputs, "[INFO] Indices found, skipping.")
 			else:
-				run_command(inputs, "%s index -t %d %s" % (inputs.sambamba,inputs.threads,s._bamTumor))
-			if(len(glob.glob(os.path.abspath(s._bamNormal + '.bai'))) > 0):
+				run_command(inputs, f"{inputs.sambamba} index -t {inputs.threads} {s._bamTumor}")
+			if len(glob.glob(os.path.abspath(f"{s._bamNormal}.bai"))) > 0:
 				print_log(inputs, "[INFO] Indices found, skipping.")
 			else:
-				run_command(inputs, "%s index -t %d %s" % (inputs.sambamba,inputs.threads,s._bamNormal))
+				run_command(inputs, f"{inputs.sambamba} index -t {inputs.threads} {s._bamNormal}")
 		elif str.upper(inputs.workflow) == "GERMLINESNVINDEL":
-			if(len(glob.glob(os.path.abspath(s._bam + '.bai'))) > 0):
+			if len(glob.glob(os.path.abspath(f"{s._bam}.bai"))) > 0:
 				print_log(inputs, "[INFO] Indices found, skipping.")
 			else:
-				run_command(inputs, "%s index -t %d %s" % (inputs.sambamba,inputs.threads,s._bam))
+				run_command(inputs, f"{inputs.sambamba} index -t {inputs.threads} {s._bam}")
 			
 	## BQSR (Step 1) ##
 	for s in targets_data_processed:
 		if str.upper(inputs.workflow) == "SOMATICSNVINDEL":
-			tumorRecalibTable = os.path.abspath(inputs.dir + '/recalib/RecalibrationTableTumor_' + s._id + '.table')
-			normalRecalibTable = os.path.abspath(inputs.dir + '/recalib/RecalibrationTableNormal_' + s._id + '.table')
-			if(os.path.exists(tumorRecalibTable) and os.path.exists(normalRecalibTable)):
+			tumorRecalibTable = os.path.abspath(f"{inputs.dir}/recalib/RecalibrationTableTumor_{s._id}.table")
+			normalRecalibTable = os.path.abspath(f"{inputs.dir}/recalib/RecalibrationTableNormal_{s._id}.table")
+			if os.path.exists(tumorRecalibTable) and os.path.exists(normalRecalibTable):
 				print_log(inputs, "[INFO] Recalibration tables found, skipping.")
-			else: ## 0 return code for success
-				if not(inputs.bed is None):
-					run_command(inputs, "java -jar %s -T BaseRecalibrator -nct %d -R %s -I %s -knownSites %s -o %s -L %s" % (inputs.gatk,inputs.threads,inputs.reference,s._bamTumor,' -knownSites '.join(inputs.knownsites),tumorRecalibTable,inputs.bed))
-				else: 
-					run_command(inputs, "java -jar %s -T BaseRecalibrator -nct %d -R %s -I %s -knownSites %s -o %s" % (inputs.gatk,inputs.threads,inputs.reference,s._bamTumor,' -knownSites '.join(inputs.knownsites),tumorRecalibTable))
-
-				s._recalibTableTumor = tumorRecalibTable
-				
-				if not(inputs.bed is None):
-					run_command(inputs, "java -jar %s -T BaseRecalibrator -nct %d -R %s -I %s -knownSites %s -o %s -L %s" % (inputs.gatk,inputs.threads,inputs.reference,s._bamNormal,' -knownSites '.join(inputs.knownsites),normalRecalibTable,inputs.bed))
+			else:
+				recalib_commands = []
+				if not inputs.bed is None:
+					recalib_commands.append(f"java -jar {inputs.gatk} -T BaseRecalibrator -nct {inputs.threads} -R {inputs.reference} -I {s._bamTumor} -knownSites {' -knownSites '.join(inputs.knownsites)} -o {tumorRecalibTable} -L {inputs.bed}")
 				else:
-					run_command(inputs, "java -jar %s -T BaseRecalibrator -nct %d -R %s -I %s -knownSites %s -o %s" % (inputs.gatk,inputs.threads,inputs.reference,s._bamNormal,' -knownSites '.join(inputs.knownsites),normalRecalibTable))
+					recalib_commands.append(f"java -jar {inputs.gatk} -T BaseRecalibrator -nct {inputs.threads} -R {inputs.reference} -I {s._bamTumor} -knownSites {' -knownSites '.join(inputs.knownsites)} -o {tumorRecalibTable}")
+				recalib_commands.append(f"java -jar {inputs.gatk} -T BaseRecalibrator -nct {inputs.threads} -R {inputs.reference} -I {s._bamNormal} -knownSites {' -knownSites '.join(inputs.knownsites)} -o {normalRecalibTable}")
+				
+				for cmd in recalib_commands:
+					run_command(inputs, cmd)
+				
+				s._recalibTableTumor = tumorRecalibTable
 				s._recalibTableNormal = normalRecalibTable
 		elif str.upper(inputs.workflow) == "GERMLINESNVINDEL":
-			RecalibTable = os.path.abspath(inputs.dir + '/recalib/RecalibrationTableNormal_' + s._id + '.table')
-			if(os.path.exists(RecalibTable)):
+			RecalibTable = os.path.abspath(f"{inputs.dir}/recalib/RecalibrationTableNormal_{s._id}.table")
+			if os.path.exists(RecalibTable):
 				print_log(inputs, "[INFO] Recalibration table found, skipping.")
 			else:
-				if not(inputs.bed is None):
-					run_command(inputs, "java -jar %s -T BaseRecalibrator -nct %d -R %s -I %s -knownSites %s -o %s -L %s" % (inputs.gatk,inputs.threads,inputs.reference,s._bam,' -knownSites '.join(inputs.knownsites),RecalibTable,inputs.bed))
+				recalib_command = ""
+				if not inputs.bed is None:
+					recalib_command = f"java -jar {inputs.gatk} -T BaseRecalibrator -nct {inputs.threads} -R {inputs.reference} -I {s._bam} -knownSites {' -knownSites '.join(inputs.knownsites)} -o {RecalibTable} -L {inputs.bed}"
 				else:
-					run_command(inputs, "java -jar %s -T BaseRecalibrator -nct %d -R %s -I %s -knownSites %s -o %s" % (inputs.gatk,inputs.threads,inputs.reference,s._bam,' -knownSites '.join(inputs.knownsites),RecalibTable))
+					recalib_command = f"java -jar {inputs.gatk} -T BaseRecalibrator -nct {inputs.threads} -R {inputs.reference} -I {s._bam} -knownSites {' -knownSites '.join(inputs.knownsites)} -o {RecalibTable}"
+				
+				run_command(inputs, recalib_command)
+				
 				s._recalibTable = RecalibTable
 				
 	## BQSR (Step 2) ##
 	for s in targets_data_processed:
 		if str.upper(inputs.workflow) == "SOMATICSNVINDEL":
-			tumorCalibBam = os.path.abspath(inputs.dir + '/bamCalib/CalibratedTumor_' + s._id + '.bam')
-			normalCalibBam = os.path.abspath(inputs.dir + '/bamCalib/CalibratedNormal_' + s._id + '.bam')
-			if(os.path.exists(tumorCalibBam) and os.path.exists(normalCalibBam)):
+			tumorCalibBam = os.path.abspath(f"{inputs.dir}/bamCalib/CalibratedTumor_{s._id}.bam")
+			normalCalibBam = os.path.abspath(f"{inputs.dir}/bamCalib/CalibratedNormal_{s._id}.bam")
+			
+			if os.path.exists(tumorCalibBam) and os.path.exists(normalCalibBam):
 				print_log(inputs, "[INFO] BQSR adjusted files found, skipping.")
 			else:
-				if not(inputs.bed is None):
-					run_command(inputs, "java -jar %s -T PrintReads -nct %d -R %s -I %s -BQSR %s -o %s -L %s" % (inputs.gatk,inputs.threads,inputs.reference,s._bamTumor,s._recalibTableTumor,tumorCalibBam,inputs.bed))
+				if inputs.bed:
+					run_command(inputs, f"java -jar {inputs.gatk} -T PrintReads -nct {inputs.threads} -R {inputs.reference} -I {s._bamTumor} -BQSR {s._recalibTableTumor} -o {tumorCalibBam} -L {inputs.bed}")
 				else:
-					run_command(inputs, "java -jar %s -T PrintReads -nct %d -R %s -I %s -BQSR %s -o %s" % (inputs.gatk,inputs.threads,inputs.reference,s._bamTumor,s._recalibTableTumor,tumorCalibBam))
+					run_command(inputs, f"java -jar {inputs.gatk} -T PrintReads -nct {inputs.threads} -R {inputs.reference} -I {s._bamTumor} -BQSR {s._recalibTableTumor} -o {tumorCalibBam}")
 				
-				if not(inputs.bed is None):
-					run_command(inputs, "java -jar %s -T PrintReads -nct %d -R %s -I %s -BQSR %s -o %s -L %s" % (inputs.gatk,inputs.threads,inputs.reference,s._bamNormal,s._recalibTableNormal,normalCalibBam,inputs.bed))
+				if inputs.bed:
+					run_command(inputs, f"java -jar {inputs.gatk} -T PrintReads -nct {inputs.threads} -R {inputs.reference} -I {s._bamNormal} -BQSR {s._recalibTableNormal} -o {normalCalibBam} -L {inputs.bed}")
 				else:
-					run_command(inputs, "java -jar %s -T PrintReads -nct %d -R %s -I %s -BQSR %s -o %s" % (inputs.gatk,inputs.threads,inputs.reference,s._bamTumor,s._recalibTableTumor,normalCalibBam))
+					run_command(inputs, f"java -jar {inputs.gatk} -T PrintReads -nct {inputs.threads} -R {inputs.reference} -I {s._bamNormal} -BQSR {s._recalibTableNormal} -o {normalCalibBam}")
+			
 			s._bamCalibTumor = tumorCalibBam
 			s._bamCalibNormal = normalCalibBam
 		elif str.upper(inputs.workflow) == "GERMLINESNVINDEL":
-			CalibBam = os.path.abspath(inputs.dir + '/bamCalib/CalibratedNormal_' + s._id + '.bam')
-			if(os.path.exists(CalibBam)):
+			CalibBam = os.path.abspath(f"{inputs.dir}/bamCalib/CalibratedNormal_{s._id}.bam")
+			
+			if os.path.exists(CalibBam):
 				print_log(inputs, "[INFO] BQSR adjusted files found, skipping.")
 			else:
-				if not(inputs.bed is None):
-					run_command(inputs, "java -jar %s -T PrintReads -nct %d -R %s -I %s -BQSR %s -o %s -L %s" % (inputs.gatk,inputs.threads,inputs.reference,s._bam,s._recalibTable,CalibBam,inputs.bed))
+				if inputs.bed:
+					run_command(inputs, f"java -jar {inputs.gatk} -T PrintReads -nct {inputs.threads} -R {inputs.reference} -I {s._bam} -BQSR {s._recalibTable} -o {CalibBam} -L {inputs.bed}")
 				else:
-					run_command(inputs, "java -jar %s -T PrintReads -nct %d -R %s -I %s -BQSR %s -o %s" % (inputs.gatk,inputs.threads,inputs.reference,s._bam,s._recalibTable,CalibBam))
+					run_command(inputs, f"java -jar {inputs.gatk} -T PrintReads -nct {inputs.threads} -R {inputs.reference} -I {s._bam} -BQSR {s._recalibTable} -o {CalibBam}")
+			
 			s._bamCalib = CalibBam
-				
+
 	return True
