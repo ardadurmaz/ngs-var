0684bdf0893884e68457fe6f139b11b1af663070
tksassano <113140378+tksassano@users.noreply.github.com>
2023-09-07 16:29:43 -0400

    Update ngs_somatic.py

--- ngs_somatic.py
+++ ngs_somatic.py
@@ -27,9 +27,9 @@ def ngs_mutect(inputs, targets_data_processed):
 			if inputs.verbose: print_log(inputs, f"[INFO] MuTect2 output file found for sample {s._id}, skipping.")
 		else:
 			if inputs.bed:
-				run_command(inputs, f"java -jar {inputs.gatk }-T MuTect2 -R {inputs.reference} -nct {inputs.threads} -I:tumor {s._bamCalibTumor} -I:normal {s._bamCalibNormal} -L {inputs.bed} -o {outFile}", f"[ERROR] Failed to run MuTect2 on files {s._bamCalibTumor} {s._bamCalibNormal}")
+				run_command(inputs, f"{inputs.gatk} -T MuTect2 -R {inputs.reference} -nct {inputs.threads} -I:tumor {s._bamCalibTumor} -I:normal {s._bamCalibNormal} -L {inputs.bed} -o {outFile}", f"[ERROR] Failed to run MuTect2 on files {s._bamCalibTumor} {s._bamCalibNormal}")
 			else:
-				run_command(inputs, f"java -jar {inputs.gatk} -T MuTect2 -R {inputs.reference} -nct {inputs.threads} -I:tumor {s._bamCalibTumor} -I:normal {s._bamCalibNormal} -o {outFile}", f"[ERROR] Failed to run MuTect2 on files {s._bamCalibTumor} {s._bamCalibNormal}")
+				run_command(inputs, f"{inputs.gatk} -T MuTect2 -R {inputs.reference} -nct {inputs.threads} -I:tumor {s._bamCalibTumor} -I:normal {s._bamCalibNormal} -o {outFile}", f"[ERROR] Failed to run MuTect2 on files {s._bamCalibTumor} {s._bamCalibNormal}")
 				
 	print_log(inputs, "\n<---> Done <--->\n")
 	return 0
